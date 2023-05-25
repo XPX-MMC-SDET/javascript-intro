844e59ebdd17a351ca741f809b24410a7d04dcb7
Martin Burolla <mburolla@xpanxion.com>
2023-05-25 12:27:52 -0400

    update

--- stu0/objects.js
+++ stu0/objects.js
@@ -4,8 +4,32 @@
 // Desc: Demonstration of objects in JavaScript.
 //
 
+const ex1 = () => {
+    console.log('Exercise 1')
+}
+
+const ex2 = () => {
+    console.log('Exercise 2')
+}
+
+const ex3 = () => {
+    console.log('Exercise 3')
+}
+
+const ex4 = () => {
+    console.log('Exercise 4')
+}
+
+const ex5 = () => {
+    console.log('Exercise 5')
+}
+
+//
+// The one and only main function.
+//
+
 const main = () => {
-    console.log('Hello World')
+    ex1() // Change to ex2(), ex3(), ex4(), or ex5().
 }
 
 main()
