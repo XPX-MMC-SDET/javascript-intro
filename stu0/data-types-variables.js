2bebc8c62417f3b26577e1522cfac7b20f8741c5
Martin Burolla <mburolla@xpanxion.com>
2023-05-25 13:45:13 -0400

    update

--- stu0/data-types-variables.js
+++ stu0/data-types-variables.js
@@ -6,22 +6,36 @@
 
 const ex1 = () => {
     console.log('Exercise 1')
+    const message = 'Hello World!'
+    console.log(message)
 }
 
 const ex2 = () => {
     console.log('Exercise 2')
+    const number1 = 5
+    const number2 = "5"
+    console.log(Number(number2) + number1)
 }
 
 const ex3 = () => {
     console.log('Exercise 3')
+    const myName = "Marty"
+    const message = " was here."
+    console.log(myName + message)
 }
 
 const ex4 = () => {
     console.log('Exercise 4')
+    const result = 1 + 2
+    console.log(typeof(result))
 }
 
 const ex5 = () => {
     console.log('Exercise 5')
+    const beginning = "JavaScript is a "
+    const typeOfLanguage = "weakly typed"
+    const end = " language"
+    console.log(beginning + typeOfLanguage + end)
 }
 
 //
