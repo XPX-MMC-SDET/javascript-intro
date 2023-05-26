b2e74a1e426c5b1d3b3e8d269f797345654196bb
Martin Burolla <mburolla@xpanxion.com>
2023-05-26 10:55:59 -0400

    update

---
+++ stu13/objects.js
@@ -0,0 +1,39 @@
+//
+// Name: objects.js
+// Date: 5/25/2023
+// Desc: Demonstration of objects in JavaScript.
+//
+
+const ex1 = () => {
+    console.log('Exercise 1')
+}
+
+const ex2 = () => {
+    console.log('Exercise 2')
+}
+
+const ex3 = () => {
+    console.log('Exercise 3')
+}
+
+const ex4 = () => {
+    console.log('Exercise 4')
+}
+
+const ex5 = () => {
+    console.log('Exercise 5')
+}
+
+//
+// YOUR FUNCTIONS GO HERE
+//
+
+//
+// The one and only main function.
+//
+
+const main = () => {
+    ex1() // Change to ex2(), ex3(), ex4(), or ex5().
+}
+
+main()
---
+++ stu3/classes.js
@@ -0,0 +1,43 @@
+//
+// Name: classes.js
+// Date: 5/25/2023
+// Desc: Demonstrate classes in JavaScript.
+//
+
+const ex1 = () => {
+    console.log('Exercise 1')
+}
+
+const ex2 = () => {
+    console.log('Exercise 2')
+}
+
+const ex3 = () => {
+    console.log('Exercise 3')
+}
+
+const ex4 = () => {
+    console.log('Exercise 4')
+}
+
+const ex5 = () => {
+    console.log('Exercise 5')
+}
+
+//
+// YOUR CLASSES GO HERE
+//
+
+//
+// YOUR FUNCTIONS GO HERE
+//
+
+//
+// The one and only main function.
+//
+
+const main = () => {
+    ex1() // Change to ex2(), ex3(), ex4(), or ex5().
+}
+
+main()
