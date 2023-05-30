a8c59c0a3e37aba84423f378ea6c7c957dfc8042
Vazquez, Rodrigo <Rodrigo.Vazquez@oliverwyman.com>
2023-05-30 10:32:53 -0600

    Exc 1

--- stu12/app.js
+++ stu12/app.js
@@ -1,2 +1,16 @@
+
+{
 let message = "Hello World"
+message ="Ola ke ase"
 console.log(message)
+let one = undefined
+let two = null
+let three
+console.log(one)
+console.log(two)
+console.log(three)
+console.log(four)
+
+}
+
+
