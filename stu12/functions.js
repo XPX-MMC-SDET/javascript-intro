3745328c68c281fb5c7ceb6ed1f29c0c5e23ed87
Vazquez, Rodrigo <Rodrigo.Vazquez@oliverwyman.com>
2023-06-01 08:55:53 -0600

    Functions _ Ex 1 to 5

--- stu12/functions.js
+++ stu12/functions.js
@@ -5,35 +5,74 @@
 //
 
 const ex1 = () => {
-    console.log('Exercise 1')
+    
+    const printMessage = (messageE1)=> console.log(messageE1)
+    printMessage("Helloo World!")
+    
 }
 
 const ex2 = () => {
-    console.log('Exercise 2')
+    const result = add(2, 3)
+    console.log(result) 
+
+    function add(arg1,arg2){
+        return arg1+arg2
+    }
 }
 
 const ex3 = () => {
-    console.log('Exercise 3')
+    
+    console.log(buildSentence('This', 'is', 'a', 'sentence'))
+    function buildSentence(arg1, arg2, arg3, arg4){
+        return arg1+" "+arg2+" "+arg3+" "+arg4
+    }
 }
 
 const ex4 = () => {
-    console.log('Exercise 4')
+    
+    const newWord = doubleIt('Hello')
+    console.log(newWord)
+
+    function doubleIt(arg1){
+        return arg1+arg1
+    }
+
 }
 
 const ex5 = () => {
-    console.log('Exercise 5')
+    
+    const multiply = (arg1,arg2)=> console.log(arg1*arg2)
+    multiply(2, 3)
+    
 }
 
-//
-// YOUR FUNCTIONS GO HERE
-//
 
-//
-// The one and only main function.
-//
 
-const main = () => {
-    ex1() // Change to ex2(), ex3(), ex4(), or ex5().
+const practice1 = () =>{
+
+    const changeStuff = (a,b,c) =>
+    {
+        a = a *10
+        b.firstName = "RODRIGO"
+        c = { firstName: "Antonio"}
+    }
+
+    let num = 10;
+    let person1 = { firstName: "Rodrigo"}
+    let person2 = { firstName: "Mathias"}
+
+    changeStuff(num, person1, person2)
+
+    console.log(num)
+    console.log(person1.firstName)
+    console.log(person2.firstName)
+
+}
+
+
+
+function main() {
+    ex5() // Change to ex2(), ex3(), ex4(), or ex5().
 }
 
 main()
