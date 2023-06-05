b2d0cfc1cc8894f720f24bd2e7af3aa3282c3af3
jack.smart <jack.smart@mmc.com>
2023-06-05 09:08:27 +0100

    functions exercise 1-5

--- stu5/functions.js
+++ stu5/functions.js
@@ -6,27 +6,53 @@
 
 const ex1 = () => {
     console.log('Exercise 1')
+    printMessage('Hello World!')
 }
 
 const ex2 = () => {
     console.log('Exercise 2')
+    const result = add(2, 3)
+    console.log(result) 
 }
 
 const ex3 = () => {
     console.log('Exercise 3')
+    console.log(buildSentence('This', 'is', 'a', 'sentence'))
 }
 
 const ex4 = () => {
     console.log('Exercise 4')
+    const newWord = doubleIt('Hello')
+    console.log(newWord)
 }
 
 const ex5 = () => {
     console.log('Exercise 5')
+    multiply(2, 3)
 }
 
 //
 // YOUR FUNCTIONS GO HERE
 //
+const printMessage = (text) => {
+    console.log(text)
+}
+
+const add = (num1, num2) => {
+    return num1 + num2
+}
+
+const buildSentence = (string1, string2, string3, string4) =>{
+    return `${string1} ${string2} ${string3} ${string4}`
+}
+
+const doubleIt = (text) =>{
+    return `${text}${text}`
+}
+
+const multiply = (num1, num2) => {
+    console.log(num1 * num2)
+}
 
 //
 // The one and only main function.
@@ -34,6 +60,10 @@ const ex5 = () => {
 
 const main = () => {
     ex1() // Change to ex2(), ex3(), ex4(), or ex5().
+    ex2()
+    ex3()
+    ex4()
+    ex5()
 }
 
 main()
