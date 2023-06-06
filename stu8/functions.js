af193b276b1b3b4a946d42c92a6220273fa9f4ea
MariaBcM <maria.becerra@oliverwyman.com>
2023-06-06 10:15:42 -0600

    Funcions  Homework 1

--- stu8/functions.js
+++ stu8/functions.js
@@ -1,39 +1,19 @@
 //
 // Name: functions.js
-// Date: 5/25/2023
+// Date: 6/6/2023
 // Desc: Demonstration of functions in JavaScript.
 //
 
-const ex1 = () => {
-    console.log('Exercise 1')
+
+function showMessage(){
+    return ( 'HI EVERYONE' );  // YOUR FUNCTIONS GO HERE
 }
 
-const ex2 = () => {
-    console.log('Exercise 2')
-}
 
-const ex3 = () => {
-    console.log('Exercise 3')
-}
-
-const ex4 = () => {
-    console.log('Exercise 4')
-}
-
-const ex5 = () => {
-    console.log('Exercise 5')
-}
 
 //
-// YOUR FUNCTIONS GO HERE
+showMessage();    // The one and only main function.
 //
 
-//
-// The one and only main function.
-//
 
-const main = () => {
-    ex1() // Change to ex2(), ex3(), ex4(), or ex5().
-}
-
-main()
+//main()
